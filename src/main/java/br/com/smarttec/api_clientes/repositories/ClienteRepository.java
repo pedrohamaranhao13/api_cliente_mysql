96aa5d5db1ded41381ba5b708fad9133749e8e90
pedrohamaranhao13 <pedrinhoelloco@outlook.com.br>
2024-09-08 20:16:58 -0300

    feature: Método create no repository.

--- src/main/java/br/com/smarttec/api_clientes/repositories/ClienteRepository.java
+++ src/main/java/br/com/smarttec/api_clientes/repositories/ClienteRepository.java
@@ -1,13 +1,27 @@
 package br.com.smarttec.api_clientes.repositories;
 
 import br.com.smarttec.api_clientes.entities.Cliente;
+import br.com.smarttec.api_clientes.factories.ConnectionFactory;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 public class ClienteRepository {
 
     public void create(Cliente cliente) throws Exception {
-        //  TODO
+
+        Connection connection = ConnectionFactory.getConnection();
+
+        PreparedStatement statement = connection.prepareStatement("INSERT INTO CLIENTE(nome, email, cpf, telefone, observacoes) VALUES(?,?,?,?,?)");
+        statement.setString(1, cliente.getNome());
+        statement.setString(2, cliente.getEmail());
+        statement.setString(3, cliente.getCpf());
+        statement.setString(4, cliente.getTelefone());
+        statement.setString(5, cliente.getObservacoes());
+        statement.execute();
+
+        statement.close();
     }
 
     public void update(Cliente cliente) throws Exception {
